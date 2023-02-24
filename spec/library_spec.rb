f8a4f71da37b335e71210921a34df5eca1d1d14f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 11:40:00 -0500

    test: add test for initialize method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,5 +1,17 @@
 require_relative 'spec_helper'
 
 describe Library do
+  before(:each) do
+    @library = Library.new('Turing Library')
+  end
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(@library).to be_a Library
+    end
+
+    it 'has a name' do
+      expect(@library.name).to eq('Turing Library')
+    end
+  end
 end
