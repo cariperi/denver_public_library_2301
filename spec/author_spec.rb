33435bb1994dc9916f4358b546d43eadc79e0729
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 11:18:33 -0500

    test: add initialize method tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,1 +1,22 @@
 require_relative 'spec_helper'
+
+describe Author do
+  before(:each) do
+    @charlotte_bronte = Author.new({first_name: "Charlotte",
+                                    last_name: "Bronte"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@charlotte_bronte).to be_a Author
+    end
+
+    it 'has a name' do
+      expect(@charlotte_bronte.name).to eq('Charlotte Bronte')
+    end
+
+    it 'starts iwth no books by default' do
+      expect(@charlotte_bronte.books).to eq([])
+    end
+  end
+end
