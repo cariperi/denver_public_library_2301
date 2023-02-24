e60af1cc6e2191443ccf350e459699e22b388510
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 12:26:22 -0500

    feat: add check out method

--- lib/library.rb
+++ lib/library.rb
@@ -20,4 +20,9 @@ class Library
     pub_years = author.books.map{|book| book.publication_year}.sort
     time_frame_hash = {start: pub_years.first, end: pub_years.last}
   end
+
+  def check_out(book)
+    return 'Sorry, we do not have this book.' if !@books.include?(book)
+    @current_loans << book
+  end
 end
