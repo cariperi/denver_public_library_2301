2424864b926d32d03ab75e6bd4569fff5aed7bd5
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 11:35:43 -0500

    feat: add write method to author class

--- lib/author.rb
+++ lib/author.rb
@@ -6,4 +6,16 @@ class Author
     @name = name[:first_name] + ' ' + name[:last_name]
     @books = []
   end
+
+  def write(title, date)
+    details = {}
+    details[:author_first_name] = @name.split(" ").first
+    details[:author_last_name] = @name.split(" ").last
+    details[:title] = title
+    details[:publication_date] = date
+
+    new_book = Book.new(details)
+    @books << new_book
+    new_book
+  end
 end
