99db373c1d7318602c6eb42987c84f5e27d303f1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 12:06:29 -0500

    feat: add publication_time_frame_for method to library class

--- lib/library.rb
+++ lib/library.rb
@@ -13,4 +13,9 @@ class Library
     author.books.each {|book| @books << book}
     @authors << author
   end
+
+  def publication_time_frame_for(author)
+    pub_years = author.books.map{|book| book.publication_year}.sort
+    time_frame_hash = {start: pub_years.first, end: pub_years.last}
+  end
 end
