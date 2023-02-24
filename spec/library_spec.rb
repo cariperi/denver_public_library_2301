065ec4746901628db93c4695fa1c075ebc873d43
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 11:49:34 -0500

    test: add tests for add author method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,6 +3,10 @@ require_relative 'spec_helper'
 describe Library do
   before(:each) do
     @library = Library.new('Turing Library')
+    @charlotte_bronte = Author.new({first_name: "Charlotte",
+                                    last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
   end
 
   describe '#initialize' do
@@ -21,5 +25,17 @@ describe Library do
     it 'has a list of authors which is empty by default' do
       expect(@library.authors).to eq([])
     end
+  end
+
+  describe '#add_author' do
+      it 'can add authors to the list of authors' do
+        expect(@library.add_author(@charlotte_bronte)).to eq([@charlotte_bronte])
+      end
+
+      it 'can add books to the list of books' do
+        @library.add_author(@charlotte_bronte)
+
+        expect(@library.books).to eq([@jane_eyre, @villette])
+      end
   end
 end
