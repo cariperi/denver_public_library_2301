720780428b5316453f61742184694c949fbcddf1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-24 11:27:54 -0500

    test: add test for write method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -19,4 +19,25 @@ describe Author do
       expect(@charlotte_bronte.books).to eq([])
     end
   end
+
+  describe '#write' do
+    before(:each) do
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      @villette = @charlotte_bronte.write("Villette", "1853")
+    end
+
+    it 'creates and returns books' do
+      expect(@jane_eyre.class).to be_a Book
+      expect(@villette.class).to be_a Book
+    end
+
+    it 'assigns attributes to book objects' do
+      expect(@jane_eyre.title).to eq('Jane Eyre')
+      expect(@villette.title).to eq('Villette')
+    end
+
+    it 'adds books to an authors list of books' do
+      expect(@charlotte_bronte.books).to eq([@jane_eyre, @villette])
+    end
+  end
 end
